4330cde2de47bad101fb4a60de984b91211b6fd6
baldyjohnmichael <baldy.johnmichael@gmail.com>
2018-06-14 09:48:46 -0400

    Passive Voice Checker now recognizes regular adverbs

--- PassiveVoiceChecker/main.py
+++ PassiveVoiceChecker/main.py
@@ -9,11 +9,17 @@ from nltk import word_tokenize
 def main():
     sentence = "the car was turned on"
     ps = "I turned the car on"
+    advPassive = "the car was quickly turned on"
+    advActive = "I quickly turned the car on"
 
     print(sentence)
     print(hasPassive(prepText(sentence)))
     print(ps)
     print(hasPassive(prepText(ps)))
+    print(advPassive)
+    print(hasPassive(prepText(advPassive)))
+    print(advActive)
+    print(hasPassive(prepText(advActive)))
 
 
 def prepText(sentence):
@@ -23,12 +29,17 @@ def prepText(sentence):
 
 # PPoV = Past Participle of verb
 # returns only whether or not a sentence has
-# the passive voice, it does not show where it is. 
+# the passive voice, it does not show where it is.
 def hasPassive(text):
     i = 0
     while i < len(text):
         if isConjOfToBE(text[i]):
+            print("to be found: " + text[i])
             if isPPoV(text[i+1]):
+                print("PPoV found: " + text[i+1])
+                return True
+            elif isADV(text[i+1]) and isPPoV(text[i+2]):
+                print("ADV found: " + text[i+1])
                 return True
         i = i + 1
     return False
@@ -45,4 +56,9 @@ def isPPoV(word):
         return True
     return False
 
+regularADV = re.compile(r"[a-z]+(ly)\b")
+def isADV(word):
+    if re.fullmatch(regularADV, word):
+        return True
+    return False
 main()
